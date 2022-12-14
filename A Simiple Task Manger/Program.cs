d49cc6cf063f65c329dfd9fe2398a169414a37d6
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-14 11:11:59 +0100

    Initial commit

---
+++ A Simiple Task Manger/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace A_Simiple_Task_Manger;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MenuCommands();
+    }
+
+    private static void MenuCommands()
+    {
+        string Menu = "Enter: \n 1: To view all Running Tasks \n 2: To Create a custom process and kill it \n 3: Check if a thread isLive or Backgroun \n 4: To Should be able to start any task \n 5: To Exit Console";
+       start: Console.Write($"***************************************\n{Menu}\n*****************************************\n ==> ");
+        string? input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                Environment.Exit(5);
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine($"{input} is an invalid Option, please Select a valid option");
+                goto start;
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# A-Simiple-Task-Manger
+ 
